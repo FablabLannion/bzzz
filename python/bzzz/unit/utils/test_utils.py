5b24e8ef2018060d9c46d9c7897ab793cdc62a43
colvert <morgan.richomme@gmail.com>
2018-11-19 22:52:58 +0100

    tests recup config
    Signed-off-by: colvert <morgan.richomme@gmail.com>

--- python/bzzz/unit/utils/test_utils.py
+++ python/bzzz/unit/utils/test_utils.py
@@ -11,6 +11,8 @@
 
 import logging
 import unittest
+import sys
+import bzzz.utils.utils as bzzz_utils
 
 
 class UtilsTestingBase(unittest.TestCase):
@@ -18,12 +20,32 @@ class UtilsTestingBase(unittest.TestCase):
     """The super class which testing classes could inherit."""
 
     logging.disable(logging.CRITICAL)
+    _config_file = sys.path[-1] + "/bzzz/conf/conf.yaml"
 
     def setUp(self):
         pass
 
-    def test_util1(self):
-        pass
+    def test_get_parameter_from_yaml(self):
+        self.assertEqual(60000,
+                         bzzz_utils.get_parameter_from_yaml(
+                            self._config_file,
+                            "general.timeout"))
+        self.assertEqual(0.80,
+                         bzzz_utils.get_parameter_from_yaml(
+                            self._config_file,
+                            "temp_sensor.gain_distant"))
+
+    def test_get_config_bad_file(self):
+        with self.assertRaises(FileNotFoundError):
+            bzzz_utils.get_parameter_from_yaml(
+                'mauvais/path',
+                'GAIN_distant')
+
+    def test_get_config_param_does_not_exist(self):
+        with self.assertRaises(ValueError):
+            bzzz_utils.get_parameter_from_yaml(
+                self._config_file,
+                "Bernard4Ever")
 
 
 if __name__ == "__main__":
--- python/bzzz/utils/utils.py
+++ python/bzzz/utils/utils.py
@@ -6,3 +6,27 @@
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
+#
+#  pylint: disable=import-error
+
+""" Utils """
+
+import yaml
+
+
+def get_parameter_from_yaml(config_file, parameter):
+    """
+    Returns the value of a given parameter in file.yaml
+    parameter must be given in string format with dots
+    Example: general.openstack.image_name
+    """
+    with open(config_file) as my_file:
+        file_yaml = yaml.safe_load(my_file)
+    my_file.close()
+    value = file_yaml
+
+    for element in parameter.split("."):
+        value = value.get(element)
+        if value is None:
+            raise ValueError("Parameter %s not defined" % parameter)
+    return value
